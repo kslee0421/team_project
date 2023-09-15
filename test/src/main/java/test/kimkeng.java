f7604c971a7f867b562cf956181c3f9856202b49
ox161472@gmail.com <ox161472@gmail.com>
2023-09-15 18:47:18 +0900

    123123

--- test/src/main/java/test/kimkeng.java
+++ test/src/main/java/test/kimkeng.java
@@ -6,7 +6,7 @@ public class kimkeng {
 		String name = "김갱";
 		String name2 = "김갱2";
 		System.out.println(name+name2); 
-		System.out.println("hello");
+		System.out.println("hello"); 123
 	}
 
 }
