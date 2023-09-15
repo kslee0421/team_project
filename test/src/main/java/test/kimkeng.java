da319e416258250ff3280530e523691cfe82bb36
kimkeng <tiggersanta@gamil.com>
2023-09-15 17:44:26 +0900

    다시

--- test/src/main/java/test/kimkeng.java
+++ test/src/main/java/test/kimkeng.java
@@ -5,7 +5,7 @@ public class kimkeng {
 	public static void main(String[] args) {
 		String name = "김갱";
 		String name2 = "김갱2";
-		System.out.println(name+name2);
+		System.out.println(name+name2); 
 	}
 
 }
